38da3ae3a37bc771e3fc5f707b125b5713f54a8f
Areyes-Trimarchi <alvaro.reyes@unitec.edu>
2015-05-15 12:07:11 -0600

    Agregar la clase FSCursorList (falta completar el codigo de los metodos)

---
+++ fscursorlist.cpp
@@ -0,0 +1,58 @@
+#include "fscursorlist.h"
+#include <iostream>
+
+FSCursorList(){
+
+}
+
+FSCursorList::~FSCursorList(){
+
+}
+
+bool FSCursorList::insert(Object*, int){
+
+}
+
+int FSCursorList::indexOf(Object*)const{
+
+}
+
+Object* FSCursorList::get(unsigned)const{
+
+}
+
+bool FSCursorList::erase(unsigned){
+
+}
+
+int FSCursorList::prev(int) const{
+
+}
+
+int FSCursorList::next(int) const{
+
+}
+
+void FSCursorList::reset(){
+
+}
+
+Object* FSCursorList::first()const{
+
+}
+
+Object* FSCursorList::last()const{
+
+}
+
+void FSCursorList::print()const{
+
+}
+
+bool FSCursorList::isFull()const{
+
+}
+
+int FSCursorList::getCapacity()const{
+	
+}
