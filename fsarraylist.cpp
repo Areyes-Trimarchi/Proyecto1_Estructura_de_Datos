db653f0326ef7312d88f7dd29e355e667ab6d643
Areyes-Trimarchi <alvaro.reyes@unitec.edu>
2015-05-15 11:10:14 -0600

    Primer Commit con todos los archivos necesarios (TDAList y clase FSArrayList)

---
+++ fsarraylist.cpp
@@ -0,0 +1,125 @@
+#include "object.h"
+#include "tdalist.h"
+#include "fsarraylist.h"
+#include <iostream>
+#include <cstdlib>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+FSArrayList::FSArrayList(){
+    data=new Object[capacity];
+    if(!data){
+        cerr<<"No hay memoria suficiente"<<endl;
+        exit(1);
+    }
+}
+FSArrayList::~FSArrayList(){
+    for (int i = 0; i < size; ++i)
+    {
+        delete data[i];
+    }
+    delete data;
+}
+
+bool FSArrayList::insert(Object* elemento, int pos) {
+    if (pos < 0 || pos > size)
+        return false;
+    if (isFull())
+        return false;
+    if(pos==size){
+        data[pos]=elemento;
+    }
+    else { 
+        for(int i = size; i>pos; i--){
+            data[i] = data[i-1];
+        }
+        data[pos] = elemento;
+    }
+    size++;
+    return true;
+}
+
+int FSArrayList::indexOf(Object* element)const {
+    int pos = -1;
+    for(int i = 0;i<size; i++){
+        if(data[i]->equals(element)){
+            pos = i;
+            break;
+        }
+    }
+    return pos;
+}
+
+Object* FSArrayList::get(unsigned pos)const {
+    if(pos<0 || pos>=size)
+        return NULL;
+    return data[pos];    
+}
+
+bool FSArrayList::remove(unsigned pos) {
+    if (pos<0 || pos>=size)
+        return NULL;
+    Object* objeto = data[pos];
+    for(int i = pos;i <(size-1); i++){
+        data[i+1] = data[i];
+    }
+    size--;
+    return objeto;  
+}
+
+int FSArrayList::prev(int pos) const {
+    return -1;
+}
+
+int FSArrayList::next(int pos) const {
+    return -1;
+}
+
+void FSArrayList::reset() {
+    for (int i = 0; i < size; ++i)
+    {
+        delete data[i];
+    } 
+    size=0;   
+}
+
+Object* FSArrayList::first()const {
+    if(isEmpty()){
+        return NULL;
+    }
+    return data[0];    
+}
+
+Object* FSArrayList::last()const {
+    if(isEmpty()){
+        return NULL;
+    }
+    return data[size-1];    
+}
+
+void FSArrayList::print()const {
+   
+}
+bool FSArrayList::isFull()const {
+    if(size==capacity){
+        return true;
+    }
+    return false;
+}
+
+bool FSArrayList::isEmpty()const {
+    if(size==0){
+        return true;
+    }
+    return false;
+}
+
+int FSArrayList::getCapacity()const{
+    return capacity;
+}
+
+int FSArrayList::getSize()const{
+    return size;
+}
---
+++ fsarraylist.h
@@ -0,0 +1,27 @@
+#ifndef FSARRAYLIST_H
+#define FSARRAYLIST_H
+
+class FSArrayList : public Object{
+	protected:
+		Object** data;
+		unsigned int capacity;
+	public:
+		FSArrayList();
+		virtual ~FSArrayList();
+		virtual bool insert(Object*, int);
+		virtual int indexOf(Object*);
+		virtual Object* get(unsigned index)const;
+		virtual bool remove(unsigned pos);
+		virtual int prev(int pos);
+		virtual int next(int pos) const;
+		virtual void reset();
+		virtual Object* first()const;	
+		virtual Object* last()const;
+		virtual void print()const;
+		virtual bool isFull()const;
+		virtual bool isEmpty()const;
+		virtual int getCapacity()const;
+		virtual int getSize()const;
+};
+
+#endif
