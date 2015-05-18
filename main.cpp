9f430103ab815075b8019a0727d7f72e553cf257
Areyes-Trimarchi <alvaro.reyes@unitec.edu>
2015-05-17 20:50:44 -0600

    Arreglar el main para poder guardar los datos en archivo de excel y errores

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "integer.h"
 #include "fscursorlist.h"
 #include "fsarraylist.h"
+#include <fstream>
 
 using namespace std;
 
@@ -28,6 +29,15 @@ int main(int argc, char *argv[])
             case 1:
             {
                 cout<<"Insertar: "<<endl;
+                ofstream datos;
+                datos.open("datos.ods");
+                datos<<"Iteracion: ";
+                for (int i = 0; i < 1000; ++i)
+                {
+                    datos<<","<<i<<",";
+                }
+                datos<<endl;
+                datos<<"Array_time: ";
                 for (int i = 0; i < 1000; ++i)
                 {
                     start=clock();
@@ -38,9 +48,13 @@ int main(int argc, char *argv[])
                         array->insert(new Integer(elemento),pos);
                     }
                     end=clock();
-                    cout<<"Array_time: "<<end-start<<" "<<endl;
+                    datos<<","<<end-start<<",";
                     array->reset();
-
+                } 
+                datos<<endl;
+                datos<<"Linked_time: ";   
+                for (int i = 0; i < 1000; ++i)
+                {
                     start=clock();
                     for (int j = 0; j < i; ++j)
                     {
@@ -49,9 +63,13 @@ int main(int argc, char *argv[])
                         linked->insert(new Integer(elemento),pos);
                     }
                     end=clock();
-                    cout<<"Linked_time: "<<end-start<<" "<<endl;
+                    datos<<","<<end-start<<",";
                     linked->reset();
-
+                }
+                datos<<endl;
+                datos<<"Cursor_time: ";
+                for (int i = 0; i < 1000; ++i)
+                {  
                     start=clock();
                     for (int j = 0; j < i; ++j)
                     {
@@ -60,238 +78,305 @@ int main(int argc, char *argv[])
                         cursor->insert(new Integer(elemento),pos);
                     }
                     end=clock();
-                    cout<<"Cursror_time: "<<end-start<<" "<<endl;
+                    datos<<","<<end-start<<",";
                     cursor->reset();
                 }
+                datos.close();
                 break;
             }    
             case 2:
             {
                cout << "IndexOf: "<<endl;
+               ofstream datos;
+               datos.open("datos.ods");
+               datos<<"Iteracion: ";
                for (int i = 0; i < 1000; ++i)
                {
-                   for (int j = 0; j < i; ++j)
-                    {
-                        elemento=rand()%1000;
-                        pos=rand()%(array->getSize()+1);
-                        array->insert(new Integer(elemento),pos);
-                        elemento=rand()%1000;
-                        pos=rand()%(linked->getSize()+1);
-                        linked->insert(new Integer(elemento),pos);
-                        elemento=rand()%1000;
-                        pos=rand()%(array->getSize()+1);
-                        cursor->insert(new Integer(elemento),pos);    
-                    }
-
+                    datos<<","<<i<<",";
+               }
+               datos<<endl;
+                datos<<"Array_time: ";
+                for (int i = 0; i < 1000; ++i)
+                {   
+                    elemento=rand()%1000;
+                    pos=rand()%(array->getSize()+1);
+                    array->insert(new Integer(elemento),pos);   
                     start=clock();
 
                     array->indexOf(new Integer(rand()%1000));
 
                     end=clock();
-                    cout<<"Array_time: "<<end-start<<" "<<endl;
+                    datos<<","<<end-start<<",";
                     array->reset();
-
+                }
+                datos<<endl;
+                datos<<"linked_time: ";
+                for (int i = 0; i < 1000; ++i)
+                {
+                    elemento=rand()%1000;
+                    pos=rand()%(linked->getSize()+1);
+                    linked->insert(new Integer(elemento),pos);
                     start=clock();
 
                     linked->indexOf(new Integer(rand()%1000));
 
                     end=clock();
-                    cout<<"linked_time: "<<end-start<<" "<<endl;
+                    datos<<","<<end-start<<",";
                     linked->reset();
-
+                }
+                datos<<endl;
+                datos<<"Cursor_time: ";
+                for (int i = 0; i < 1000; ++i)
+                {
+                    elemento=rand()%1000;
+                    pos=rand()%(array->getSize()+1);
+                    cursor->insert(new Integer(elemento),pos);
                     start=clock();
 
                     cursor->indexOf(new Integer(rand()%1000));
 
                     end=clock();
-                    cout<<"Cursor_time: "<<end-start<<" "<<endl;
+                    datos<<","<<end-start<<",";
                     cursor->reset();
-               }
+                }  
+                datos.close();  
                break;
             }   
             case 3:
             {
-               cout << "Remove: "<<endl;
-               for (int i = 0; i < 1000; ++i)
-               {
-                   for (int j = 0; j < i; ++j)
-                    {
-                        elemento=rand()%1000;
-                        pos=rand()%(array->getSize()+1);
-                        array->insert(new Integer(elemento),pos);
-                        elemento=rand()%1000;
-                        pos=rand()%(linked->getSize()+1);
-                        linked->insert(new Integer(elemento),pos);
-                        elemento=rand()%1000;
-                        pos=rand()%(array->getSize()+1);
-                        cursor->insert(new Integer(elemento),pos);  
-                    }
-
+                cout << "Remove: "<<endl;
+                ofstream datos;
+                datos.open("datos.ods");
+                datos<<"Iteracion: ";
+                for (int i = 0; i < 1000; ++i)
+                {
+                    datos<<","<<i<<",";
+                }
+                datos<<endl;
+                datos<<"Array_time: ";
+                for (int i = 0; i < 1000; ++i)
+                {
+                    elemento=rand()%1000;
+                    pos=rand()%(array->getSize()+1);
+                    array->insert(new Integer(elemento),pos);
                     start=clock();
 
-                    array->remove(rand()%999);
+                    array->remove(0);
 
                     end=clock();
-                    cout<<"Array_time: "<<end-start<<" "<<endl;
+                    datos<<","<<end-start<<",";
                     array->reset();
-
+                }
+                datos<<endl;
+                datos<<"linked_time: ";
+                for (int i = 0; i < 1000; ++i)
+                {
+                    elemento=rand()%1000;
+                    pos=rand()%(linked->getSize()+1);
+                    linked->insert(new Integer(elemento),pos);
                     start=clock();
                     
-                    linked->remove(rand()%999);
+                    linked->remove(0);
                     
                     end=clock();
-                    cout<<"linked_time: "<<end-start<<" "<<endl;
+                    datos<<","<<end-start<<",";
                     linked->reset();
-
+                }
+                datos<<endl;
+                datos<<"Cursor_time: ";
+                for (int i = 0; i < 1000; ++i)
+                {
+                    elemento=rand()%1000;
+                    pos=rand()%(array->getSize()+1);
+                    cursor->insert(new Integer(elemento),pos);
                     start=clock();
                     
-                    cursor->remove(rand()%999);
+                    cursor->remove(0);
                     
                     end=clock();
-                    cout<<"Cursor_time: "<<end-start<<" "<<endl;
+                    datos<<","<<end-start<<",";
                     cursor->reset();
                }
+               datos.close();
                break;
             }   
             case 4:
             {
-               cout << "Get: "<<endl;
-               for (int i = 0; i < 1000; ++i)
-               {
-                   for (int j = 0; j < i; ++j)
-                    {
-                        elemento=rand()%1000;
-                        pos=rand()%(array->getSize()+1);
-                        array->insert(new Integer(elemento),pos);
-                        elemento=rand()%1000;
-                        pos=rand()%(linked->getSize()+1);
-                        linked->insert(new Integer(elemento),pos);
-                        elemento=rand()%1000;
-                        pos=rand()%(array->getSize()+1);
-                        cursor->insert(new Integer(elemento),pos);   
-                    }
-
+                cout << "Get: "<<endl;
+                ofstream datos;
+                datos.open("datos.ods");
+                datos<<"Iteracion: ";
+                for (int i = 0; i < 1000; ++i)
+                {
+                    datos<<","<<i<<",";
+                }
+                datos<<endl;
+                datos<<"Array_time: ";
+                for (int i = 0; i < 1000; ++i)
+                {
+                    elemento=rand()%1000;
+                    pos=rand()%(array->getSize()+1);
+                    array->insert(new Integer(elemento),pos); 
+                    elemento=rand()%1000;
+                    pos=rand()%(linked->getSize()+1);
+                    linked->insert(new Integer(elemento),pos);
+                    elemento=rand()%1000;
+                    pos=rand()%(array->getSize()+1);
+                    cursor->insert(new Integer(elemento),pos);
+                }
+                for (int i = 0; i < 1000; ++i)
+                {
                     start=clock();
 
-                    array->get(rand()%999);
+                    array->get(rand()%899);
 
                     end=clock();
-                    cout<<"Array_time: "<<end-start<<" "<<endl;
-                    array->reset();
-
+                    datos<<","<<end-start<<",";
+                }
+                datos<<endl;
+                datos<<"linked_time: ";
+                for (int i = 0; i < 1000; ++i)
+                {
                     start=clock();
 
-                    linked->get(rand()%999);
+                    linked->get(rand()%899);
                     
                     end=clock();
-                    cout<<"linked_time: "<<end-start<<" "<<endl;
-                    linked->reset();
-
+                    datos<<","<<end-start<<",";
+                }
+                datos<<endl;
+                datos<<"Cursor_time: "; 
+                for (int i = 0; i < 1000; ++i)
+                {   
                     start=clock();
                     
-                    cursor->get(rand()%999);
+                    cursor->get(0);
                     
                     end=clock();
-                    cout<<"Cursor_time: "<<end-start<<" "<<endl;
-                    cursor->reset();
-               }
-               break;
+                    datos<<","<<end-start<<",";
+                }
+                datos.close();
+                break;
             }   
             case 5:
             {
-               cout << "First: "<<endl;
-               for (int i = 0; i < 1000; ++i)
-               {
-                   for (int j = 0; j < i; ++j)
-                    {
-                        elemento=rand()%1000;
-                        pos=rand()%(array->getSize()+1);
-                        array->insert(new Integer(elemento),pos);
-                        elemento=rand()%1000;
-                        pos=rand()%(linked->getSize()+1);
-                        linked->insert(new Integer(elemento),pos);
-                        elemento=rand()%1000;
-                        pos=rand()%(array->getSize()+1);
-                        cursor->insert(new Integer(elemento),pos);   
-                    }
-
+                cout << "First: "<<endl;
+                ofstream datos;
+                datos.open("datos.ods");
+                datos<<"Iteracion: ";
+                for (int i = 0; i < 1000; ++i)
+                {
+                    datos<<","<<i<<",";
+                }
+                datos<<endl;
+                datos<<"Array_time: ";
+                for (int i = 0; i < 1000; ++i)
+                {
+                    elemento=rand()%1000;
+                    pos=rand()%(array->getSize()+1);
+                    array->insert(new Integer(elemento),pos);
                     start=clock();
 
                     array->first();
 
                     end=clock();
-                    cout<<"Array_time: "<<end-start<<" "<<endl;
+                    datos<<","<<end-start<<",";
                     array->reset();
-
+                }
+                datos<<endl;
+                datos<<"linked_time: ";
+                for (int i = 0; i < 1000; ++i)
+                {
+                    elemento=rand()%1000;
+                    pos=rand()%(linked->getSize()+1);
+                    linked->insert(new Integer(elemento),pos);
                     start=clock();
                     
                     linked->first();
                     
                     end=clock();
-                    cout<<"linked_time: "<<end-start<<" "<<endl;
+                    datos<<","<<end-start<<",";
                     linked->reset();
-
+                }
+                datos<<endl;
+                datos<<"Cursor_time: ";
+                for (int i = 0; i < 1000; ++i)
+                {
+                    elemento=rand()%1000;
+                    pos=rand()%(array->getSize()+1);
+                    cursor->insert(new Integer(elemento),pos);
                     start=clock();
                     
                     cursor->first();
                     
                     end=clock();
-                    cout<<"Cursor_time: "<<end-start<<" "<<endl;
+                    datos<<","<<end-start<<",";
                     cursor->reset();
-               }
-               break;
+                }
+                datos.close();
+                break;
             }   
             case 6:
             {
-               cout << "Last: "<<endl;
-               for (int i = 0; i < 1000; ++i)
-               {
-                   for (int j = 0; j < i; ++j)
-                    {
-                        elemento=rand()%1000;
-                        pos=rand()%(array->getSize()+1);
-                        array->insert(new Integer(elemento),pos);
-                        elemento=rand()%1000;
-                        pos=rand()%(linked->getSize()+1);
-                        linked->insert(new Integer(elemento),pos);
-                        elemento=rand()%1000;
-                        pos=rand()%(array->getSize()+1);
-                        cursor->insert(new Integer(elemento),pos);    
-                    }
-
+                cout << "Last: "<<endl;
+                ofstream datos;
+                datos.open("datos.ods");
+                datos<<"Iteracion: ";
+                for (int i = 0; i < 1000; ++i)
+                {
+                    datos<<","<<i<<",";
+                }
+                datos<<endl;
+                datos<<"Array_time: ";
+                for (int i = 0; i < 1000; ++i)
+                {
+                    elemento=rand()%1000;
+                    pos=rand()%(array->getSize()+1);
+                    array->insert(new Integer(elemento),pos);
                     start=clock();
 
                     array->last();
 
                     end=clock();
-                    cout<<"Array_time: "<<end-start<<" "<<endl;
+                    datos<<","<<end-start<<",";
                     array->reset();
-
+                }
+                datos<<endl;
+                datos<<"linked_time: ";
+                for (int i = 0; i < 1000; ++i)
+                {
+                    elemento=rand()%1000;
+                    pos=rand()%(linked->getSize()+1);
+                    linked->insert(new Integer(elemento),pos);
                     start=clock();
                     
                     linked->last();
                     
                     end=clock();
-                    cout<<"linked_time: "<<end-start<<" "<<endl;
+                    datos<<","<<end-start<<",";
                     linked->reset();
-
+                }
+                datos<<endl;
+                datos<<"Cursor_time: ";
+                for (int i = 0; i < 1000; ++i)
+                {
+                    elemento=rand()%1000;
+                    pos=rand()%(array->getSize()+1);
+                    cursor->insert(new Integer(elemento),pos);
                     start=clock();
                     
                     cursor->last();
                     
                     end=clock();
-                    cout<<"Cursor_time: "<<end-start<<" "<<endl;
+                    datos<<","<<end-start<<",";
                     cursor->reset();
                }
+               cout<<"3. "<<endl;
+               datos.close();
                break;
-            }   
-            case 7:
-            {
-               cout << "Salir"<<endl;
-               flag=-1;
-               break; 
-            }        
-        }      
+            }          
+        }  
+        flag=menu();   
     }
     delete array;
     delete linked;
